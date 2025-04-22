93a2676930a70ff4657a5389a5d4d40ee1b2577c
KAnorov <kostya-anorov046@yandex.ru>
2025-04-22 14:25:06 +0300

    1111

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,26 +1,27 @@
-// import { ChooseProductModal } from "@/shared/components/shared";
-// import { prisma } from "@/prisma/prisma-client";
-// import { notFound } from "next/navigation";
-// export const dynamic = 'force-dynamic';
-// export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
-//   const resolvedParams = await params;
-//   const id = Number(resolvedParams.id);
+import { ChooseProductModal } from "@/shared/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+export const dynamic = 'force-dynamic';
+export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
+  const id = Number(resolvedParams.id);
 
-//   if (isNaN(id)) {
-//     notFound();
-//   }
+  if (isNaN(id)) {
+    notFound();
+  }
 
-//   const product = await prisma.product.findUnique({
-//     where: { id },
-//     include: {
-//       ingredient: true,
-//       items: true,
-//     }
-//   });
+  const product = await prisma.product.findUnique({
+    where: { id },
+    include: {
+      ingredient: true,
+      items: true,
+           
+    }
+  });
 
-//   if (!product) {
-//     notFound();
-//   }
+  if (!product) {
+    notFound();
+  }
 
-//   return <ChooseProductModal product={product} />;
-// }
+  return <ChooseProductModal product={product} />;
+}
