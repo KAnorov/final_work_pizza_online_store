99169c6aff7ea5db354f17fd52e0d9f2e27229ae
KAnorov <kostya-anorov046@yandex.ru>
2025-04-23 22:39:42 +0300

    +

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,8 @@
+import { authOptions } from "@/shared/lib/authOptions";
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' }
-      },
-      async authorize(credentials, req) {
-        console.log('________ credentials', credentials, req);
 
-        if ('1' === credentials?.username && '1' === credentials.password)
-          return { id: '11111', name: 'J Smith', email: 'jsmith@example.com' };
-        return null;
-      }
-    })
-
-  ],
-
-}
+ 
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
